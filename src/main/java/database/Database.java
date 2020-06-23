2f3ee703e9c300c4fa0e7ad2624807d1b0dc3250
tdutyui <66724095+tdutyui@users.noreply.github.com>
2020-06-23 14:37:20 +0500

    Add files via upload

---
+++ src/main/java/database/Database.java
@@ -0,0 +1,60 @@
+package database;
+
+import java.io.*;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Database {
+    public static void main(String[] args) throws IOException, SQLException {
+        File file = new File("data/prices.csv");
+        String url = "jdbc:postgresql://localhost:5432/OilPrice";
+        Connection connection = DriverManager.getConnection(url, "postgres", "dekebikil");
+
+        String line = "";
+        int count = 0;
+        double price = 0;
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while ((line = br.readLine()) != null) {
+            if (count == 0) {
+                count++;
+                continue;
+            }
+            String parsedLine = line.replaceAll("\"", "");
+            String[] month = parsedLine.split(",");
+            LocalDate startDate = formatDate(month[0]);
+            LocalDate endDate = formatDate(month[1]);
+
+            if (month.length>3) {
+                price = Double.parseDouble(month[2]+"."+month[3]);
+            }
+            else {
+                price = Double.parseDouble(month[2]);
+            }
+
+            connection.createStatement();
+            String sql = "insert into oilprice (начальная_дата, конечная_дата, цена) values (?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setObject(1, startDate);
+            statement.setObject(2, endDate);
+            statement.setObject(3, price);
+            statement.executeUpdate();
+        }
+    }
+
+    public static LocalDate formatDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MMM.yy", Locale.getDefault());
+        LocalDate formatted = null;
+
+        if (!date.contains("май")) {
+            formatted = LocalDate.parse(date, formatter);
+        }
+        else {
+            String fixed = date.replace("май", "мая");
+            formatted = LocalDate.parse(fixed, formatter);
+        }
+        return formatted;
+    }
+}
---
+++ src/main/java/oilPrice/Controller.java
@@ -0,0 +1,82 @@
+package oilPrice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.*;
+import java.sql.Date;
+import java.util.*;
+
+@RestController
+public class Controller {
+
+    private String url = "jdbc:postgresql://localhost:5432/OilPrice";
+    private Connection connection = DriverManager.getConnection(url, "postgres", "dekebikil");
+
+    public Controller() throws SQLException {
+    }
+
+    @RequestMapping(path = "/data")
+    @GetMapping
+    public List<Map<String, Object>> allData() throws SQLException {
+        List<Map<String, Object>> data = new ArrayList<>();
+        PreparedStatement statement = connection.prepareStatement("select * from oilprice");
+        ResultSet rs = statement.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        while (rs.next()) {
+            Map<String, Object> row = new LinkedHashMap<>();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                row.put(rsmd.getColumnLabel(i), rs.getObject(i));
+                data.add(row);
+            }
+        return data;
+    }
+
+    @RequestMapping(path = "/data/{date}")
+    @GetMapping
+    public double priceOnDate(@PathVariable Date date) throws SQLException {
+        double price = 0;
+        PreparedStatement statement = connection.prepareStatement("select price from oilprice where end_date ='"+date+"'");
+        ResultSet rs = statement.executeQuery();
+
+        if (rs.next()) {
+            price = rs.getDouble(1);
+        }
+        return price;
+    }
+
+    @RequestMapping(path = "/data/{start}/{end}")
+    @GetMapping
+    public double priceOnTimeInterval(@PathVariable Date start, @PathVariable Date end) throws SQLException {
+        double avgPrice = 0;
+        PreparedStatement statement = connection.prepareStatement("select avg(price) from oilprice where end_date between '"+start+"' and '"+end+"'");
+        ResultSet rs = statement.executeQuery();
+
+        if (rs.next()) {
+            avgPrice = rs.getDouble(1);
+        }
+        return avgPrice;
+    }
+
+    @RequestMapping(path = "/data/price/{start}/{end}")
+    @GetMapping
+    public HashMap<String, Double> minAndMaxPrice(@PathVariable Date start, @PathVariable Date end) throws SQLException {
+        double minPrice = 0;
+        double maxPrice = 0;
+        HashMap<String, Double> minAndMax = new HashMap<String, Double>();
+
+        PreparedStatement statement = connection.prepareStatement("select min(price), max(price) from oilprice where end_date between '"+start+"' and '"+end+"'");
+        ResultSet rs = statement.executeQuery();
+        if (rs.next()) {
+            minPrice = rs.getDouble(1);
+            maxPrice = rs.getDouble(2);
+        }
+
+        minAndMax.put("min",minPrice);
+        minAndMax.put("max",maxPrice);
+        return minAndMax;
+    }
+}
